c2405b8fd809e4d6df0de992ebc5853927033df6
estefaniari <estefanilorenzo49@gmail.com>
2024-04-05 23:26:31 -0400

    Cambios

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ from app import area_triangulo
 class TestAreaTriangulo(unittest.TestCase):
 
     def test_area_triangulo(self):
-        self.assertEqual(area_triangulo(3, 4), 6)
+        self.assertEqual(area_triangulo(3, 4), 6) 
