427e6241722f1b6a2e11f1cc1f6dab650c74bd88
estefaniari <estefanilorenzo49@gmail.com>
2024-04-12 19:07:13 -0400

    Realizar cambios en la rama dev

--- test_app.py
+++ test_app.py
@@ -6,3 +6,11 @@ class TestAreaTriangulo(unittest.TestCase):
 
     def test_area_triangulo(self):
         self.assertEqual(area_triangulo(3, 4), 6) 
+    
+    def test_area_triangulo_base_negativa(self):
+        with self.assertRaises(ValueError):
+            area_triangulo(-3, 4)
+
+    def test_area_triangulo_altura_cero(self):
+        with self.assertRaises(ValueError):
+            area_triangulo(3, 0) 
